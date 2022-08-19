7b61cbb6498998e1fda2c30cfa3e1f65f403b73a
Guillem <guillem.mart7@gmail.com>
2022-08-19 20:14:45 +0200

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState } from 'react'
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
@@ -6,13 +7,14 @@ import {Routes, Route} from "react-router-dom"
 import countriesJSON from "./countries.json"
 
 function App() {
+  const [countries, setCountries] = useState(countriesJSON)
   return (
     <div className="App">
       <Navbar/>
       <Routes>
         <Route path=":alpha3Code" element={<CountryDetails countriesJSON={countriesJSON}/>}></Route>
       </Routes>
-      <CountriesList/>
+      <CountriesList countries={countries}/>
     </div>
   );
 }
